c2a6bde6f195e1a65d2310dd1b83160b3816e508
KANGPENG SHAO <billowen035@gmail.com>
2015-01-19 16:48:19 +0800

    daily commit'

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <fstream>
-#include "src/library.h"
-#include "src/exceptions.h"
+#include "library.h"
+#include "exceptions.h"
 
 
 int main()
 {
     try
     {
-        GDS::Library lib;
+        GDS::Library lib1;
+		std::ifstream in;
+		std::ofstream out;
+		in.open("rotate_90_test.ics.db", std::ios_base::binary);
+		lib1.read(in);
+		in.close();
+		out.open("origin.txt");
+		lib1.printASCII(out);
+		out.close();
+		out.open("tmp.db", std::ios_base::binary);
+		lib1.write(out);
+		out.close();
+
+		GDS::Library lib2;
+		in.open("tmp.db", std::ios_base::binary);
+		lib2.read(in);
+		in.close();
+		out.open("after.txt");
+		lib2.printASCII(out);
+		out.close();
         
     }
     catch (GDS::FormatError &e)
